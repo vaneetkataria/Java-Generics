f8c2a1b6fe25eb0ab40f54f19f3d60931e9d334a
vaneet <vaneetkataria54@gmail.com>
2018-12-02 04:01:14 +0530

    Chnages for Lower bounded wildcards

--- src/main/java/com/katariasoft/technologies/generics/types/SurfaceAreaAndVolumesFunctions.java
+++ src/main/java/com/katariasoft/technologies/generics/types/SurfaceAreaAndVolumesFunctions.java
@@ -6,21 +6,19 @@ public class SurfaceAreaAndVolumesFunctions {
 
 	private TriFunction<Integer, Integer, Double, Double> paintingCostCalculator = paintingCostCalculatorDef();
 	private Function<Double, Double> distamperChargesCalculator = d -> d / 10;
+	private TriFunction<Integer, Integer, Double, Double> finalDistemperChargesCalculator = paintingCostCalculator
+			.andThen(distamperChargesCalculator);
 
 	private TriFunction<Integer, Integer, Double, Double> paintingCostCalculatorDef() {
 		return (l, b, r) -> l * b * r;
 	}
 
-	public TriFunction<Integer, Integer, Double, Double> getPaintingCostCalculator() {
-		return paintingCostCalculator;
-	}
-
 	public static void main(String args[]) {
 		SurfaceAreaAndVolumesFunctions functions = new SurfaceAreaAndVolumesFunctions();
 		System.out.println(
-				"Paint charges for 10 , 10 and 5.5 is :" + functions.getPaintingCostCalculator().apply(10, 10, 5.5));
+				"Paint charges for 10 , 10 and 5.5 is :" + functions.paintingCostCalculator.apply(10, 10, 5.5));
 		System.out.println("Distamper Charges for 10 , 10 , 5.5 is "
-				+ functions.paintingCostCalculator.andThen(functions.distamperChargesCalculator).apply(10, 10, 5.5));
+				+ functions.finalDistemperChargesCalculator.apply(10, 10, 5.5));
 
 	}
 
